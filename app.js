a780c8efbeb8505f2b9e20a82359c59b55343f39
Sean Urgel <seantheurgel@gmail.com>
2018-08-19 22:47:18 +0800

    Verified IdToken

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 const { ApolloServer, gql } = require('apollo-server');
+const admin = require('firebase-admin')
+const firebase = require('firebase')
+const token = require('./token')
+const serviceAccount = require('./serviceAccount.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const context = async ({req}) => {
+
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(token)
+    console.log(decodedToken)
+  } catch (error) {
+    console.error(error)
+  }
+
+}
 
 // The GraphQL schema
 const typeDefs = gql`
@@ -15,13 +34,22 @@ const typeDefs = gql`
 // A map of functions which return data for the schema.
 const resolvers = {
   Query: {
-    hello: () => 'world'
+    hello: (parent, args, context) => {
+      // console.log(context)
+      return 'world'
+    }
   }
 };
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context,
+  playground: {
+    settings: {
+      'editor.cursorShape': 'line' // possible values: 'line', 'block', 'underline'
+    }
+  }
 });
 
 server.listen().then(({ url }) => {
